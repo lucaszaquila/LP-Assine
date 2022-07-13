e98cb0294f457cf4c2520cb5a8168db94985e6ae
Lucas Áquila <lucaszaquila@gmail.com>
2022-07-13 15:57:37 -0300

    Index improved script

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,12 @@
-const printed = document.querySelector('#printed');
-const digital = document.querySelector('#digital');
-
-function showDetails(target) {
-    if(target === 'printed') {
-        printed.classList.toggle('active');
-
-        setTimeout(function() {
-            printed.querySelector('.details').style.transform = printed.classList.contains('active') ? 
-            'translateY(0%)' : 'translateY(-100%)';
+document.addEventListener('DOMContentLoaded', function (){
+    document.querySelectorAll('.options button').forEach(function (elem){
+        elem.addEventListener('click',function (e){
+            const container = document.querySelector('[data-type="'+e.currentTarget.dataset.subscription+'"]')
+            container.classList.toggle('active')
+            setTimeout(function() {
+                container.querySelector('.details').style.transform = container.classList.contains('active') ?
+                    'translateY(0%)' : 'translateY(-100%)';
+            })
         })
-    }
-    if(target === 'digital') {
-        digital.classList.toggle('active');
-
-        setTimeout(function() {
-            digital.querySelector('.details').style.transform = digital.classList.contains('active') ? 
-            'translateY(0%)' : 'translateY(-100%)';
-        })
-    }
-}
+    })
+})
