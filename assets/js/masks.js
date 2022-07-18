a408b71a58e3c9a7d5d8b64c6b887687593560ca
Lucas Áquila <lucaszaquila@gmail.com>
2022-07-18 12:02:17 -0300

    Card lenght fix

--- assets/js/masks.js
+++ assets/js/masks.js
@@ -16,14 +16,7 @@ const masks = {
             .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
             .replace(/(-\d{4})\d+?$/, '$1')
     },
-    card (value) {
-        return value
-            .replace(/\D+/g, '')
-            .replace(/(\d{4})(\d)/, '$1 ($2)')
-            .replace(/(\d{4})(\d)/, '$1 $2')
-            .replace(/(-\d{2})\d+?$/, '$1')
-    },
-
+    
     cep (value) {
         return value
             .replace(/\D+/g, '')
