eeaa114921367ea8a3e3cc2a1a65c4df66b71398
Lucas Áquila <lucaszaquila@gmail.com>
2022-07-18 11:07:31 -0300

    Polishing

--- assets/js/address.js
+++ assets/js/address.js
@@ -1,40 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const cepField = document.getElementById('cep')
+    document.querySelectorAll('[data-addresstype]').forEach(function (elem) {
+        elem.addEventListener('input',function() {
+            const addressType = elem.dataset.addresstype;
+            if(elem.value.length === 8 && !isNaN(elem.value)){
+                const xhr = new XMLHttpRequest();
+                const url= "https://viacep.com.br/ws/"+elem.value+"/json/"
 
-    cepField.addEventListener('input',function() {
-        if(cepField.value.length === 8 && !isNaN(cepField.value)){
-            const xhr = new XMLHttpRequest();
-            const url= "https://viacep.com.br/ws/"+cepField.value+"/json/"
+                xhr.open('GET',url)
+                xhr.onreadystatechange = function() {
+                    if(xhr.readyState===4){
+                        if(xhr.status ===200){
+                            const address = JSON.parse(xhr.responseText)
+                            if(!address.erro){
 
-            xhr.open('GET',url)
-            xhr.onreadystatechange = function() {
-                if(xhr.readyState===4){
-                    if(xhr.status ===200){
-                        const address = JSON.parse(xhr.responseText)
-                        if(!address.erro){
-                            for (let fields in address){
-                                const field = document.querySelector('[data-field="'+fields+'"]');
-                                if(field){
-                                    field.value = address[fields]
+                                for (let fields in address){
+                                    const field = document.querySelector('[data-address="'+addressType+'"][data-field="'+fields+'"]');
+                                    if(field){
+                                        field.value = address[fields]
+                                    }
                                 }
+                                return;
                             }
-                            return;
+                            console.log('Cep Não Encontrado!')
+                        }else{
+                            console.log('ocorreu um erro durante a busca do CEP')
                         }
-                        console.log('Cep Não Encontrado!')
-                    }else{
-                        console.log('ocorreu um erro durante a busca do CEP')
                     }
                 }
+                xhr.send();
+            }else{
+                resetForm(addressType)
             }
-            xhr.send();
-        }else{
-            resetForm()
-        }
+        })
     })
 });
 
-function resetForm(){
-    document.querySelectorAll('[data-reset="true"]').forEach(function (elem){
+function resetForm(form){
+    document.querySelectorAll('[data-address="'+form+'"][data-reset="true"]').forEach(function (elem){
         elem.value='';
     });
 }
